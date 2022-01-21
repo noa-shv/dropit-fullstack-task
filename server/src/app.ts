370674b91cef3b98ee3e2e66d641083a7e9b295d
noa-shv <noashv2501@gmail.com>
2022-01-21 11:03:11 +0200

    add basic server config and API endpoints

---
+++ server/src/app.ts
@@ -0,0 +1,72 @@
+import express from 'express';
+const app = express();
+const port = 8080;
+
+const categories = require('./data/categories');
+const products = require('./data/products');
+let cart = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello World! aacjhjfgdxvxva');
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/products', (req, res) => {
+    try {
+        const {offset=0, limit=products.length, category=[], title, sort} = req.query;
+        let filteredProducts = [];
+
+        products.forEach((product, index) => {
+            if(index >= offset && index <= limit) {
+                let pushToFilteredProducts = true;
+
+                if(category.length > 0) {
+                    pushToFilteredProducts =
+                        pushToFilteredProducts && (category as string[]).includes(product.category);
+                }
+                if(title) {
+                    pushToFilteredProducts = pushToFilteredProducts && product.title.includes(title);
+                }
+                if(pushToFilteredProducts) {
+                    filteredProducts.push(product);
+                }
+            }
+        })
+
+        res.send(filteredProducts);
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+app.get('/product/:id', (req, res) => {
+    const { id } = req.params;
+    // TODO: add err for when not found
+    try {
+        const foundProduct = products.find(product => product.id == id);
+        res.send(foundProduct);
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+app.get('/orders', (req, res) => {
+    res.send(cart);
+});
+
+app.get('/ordersNumber', (req, res) => {
+    res.send(cart.length);
+});
+
+app.post('/addOrder', (req, res) => {
+    // TODO: add typechecking
+    const item = req.body;
+    cart.push(item);
+});
+
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
---
+++ server/src/data/categories.ts
@@ -0,0 +1,19 @@
+// export = [
+//     {
+//         id: 1,
+//         name: "men's clothing"
+//     },
+//     {
+//         id: 2,
+//         name: "jewelery"
+//     },
+//     {
+//         id: 3,
+//         name: "electronics"
+//     },
+//     {
+//         id: 4,
+//         name: "women's clothing"
+//     },
+// ];
+export = ["men's clothing", "jewelery", "electronics", "women's clothing"];
