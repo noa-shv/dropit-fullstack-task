80fc64dd93da88a38478463d3c53b4d8ec8220c3
noa-shv <noashv2501@gmail.com>
2022-01-22 19:08:58 +0200

    add catalogcard component

--- client/src/modules/catalog/CatalogCard.tsx
+++ client/src/modules/catalog/CatalogCard.tsx
@@ -1,11 +1,32 @@
-import {Card, CardContent} from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-function CatalogView() {
+interface Props {
+  img: string;
+  title: string;
+  price: number;
+};
+
+function CatalogView({img, title, price}: Props) {
     return(
-        <Card>
-            <CardContent>
-                fuc
-            </CardContent>
+        <Card sx={{height: "75vh"}}>
+                <CardMedia
+                    component="img"
+                    height="auto"
+                    image={img}
+                    alt="green iguana"
+                    sx={{
+                        maxHeight: "72%",
+                        maxWidth: "100%",
+                        objectFit: "contain"
+                    }}
+                />
+        <CardContent>
+                <Typography gutterBottom variant="subtitle1" component="div">{title}</Typography>
+                <Typography gutterBottom variant="h6" component="div">ILS{price}</Typography>
+    </CardContent>
         </Card>
     );
 }
--- client/src/modules/catalog/CatalogView.tsx
+++ client/src/modules/catalog/CatalogView.tsx
@@ -4,14 +4,24 @@ import {Container, Grid} from '@mui/material';
 import CatalogCard from './CatalogCard';
 import CatalogFilters from './CatalogFilters';
 
+const product = {
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+}
+
 function CatalogView() {
     return (
         <div>
             <CatalogFilters />
             <Container>
                 <Grid container>
-                    <Grid item>
-                        <CatalogCard />
+                    <Grid item xs={3}>
+                        <CatalogCard
+                            title={product.title}
+                            img={product.image}
+                            price={product.price}
+                        />
                     </Grid>
                 </Grid>
             </Container>
